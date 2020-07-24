fe89dedee2bbdff1c99fd816bed62138f4c42f98
aliaslazaris <aliaslazaris@gmail.com>
2020-07-24 16:54:14 -0500

    added some changes

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {}
 resource "aws_instance" "jenkins" {
-  ami           = "${aws_ami_id}"
+  ami           = "${aws_ami_ids}"
   instance_type = "t2.micro"
   tags = {
-    Name = "${aws_instance_name}"
+    Name = "${aws_instance}"
   }
 }
 
-aws_ami_id = "ami-08f3d892de259504d"
-aws_instance_name = "Jenkins"
+aws_ami_ids = "ami-08f3d892de259504d"
+aws_instance = "Jenkins"
