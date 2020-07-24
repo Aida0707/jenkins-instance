2bfa785771af311e394506092561d7f2ab596bf6
aliaslazaris <aliaslazaris@gmail.com>
2020-07-24 17:05:02 -0500

    added changes inside main.tfvars

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {}
 resource "aws_instance" "jenkins" {
-  ami           = "${aws_ami_ids}"
+  ami           = "${aws_ami_id}"
   instance_type = "t2.micro"
   tags = {
-    Name = "${aws_instance}"
+    Name = "${aws_instance_name}"
   }
 }
 
-aws_ami_ids = "ami-08f3d892de259504d"
-aws_instance = "Jenkins"
+aws_ami_id = "ami-08f3d892de259504d"
+aws_instance_name = "Jenkins"
