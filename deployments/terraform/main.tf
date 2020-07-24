6881a47301f6cc5da05928404d8392d66c11441f
aliaslazaris <aliaslazaris@gmail.com>
2020-07-24 17:37:29 -0500

    added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {}
 resource "aws_instance" "jenkins" {
-  ami           = "${aws_ami_id}"
+  ami           = "${var.aws_ami_id}"
   instance_type = "t2.micro"
   tags = {
-    Name = "${aws_instance_name}"
+    Name = "${var.aws_instance_name}"
   }
 }
 
